8e1a25ea5e7ed514ba96539ad321a1156d9c24fb
nir905 <nira@welldone-software.com>
2020-03-18 13:16:11 +0200

    add additionalIgnoreElements + fix readme

---
+++ test/fixtures/options-additionalIgnoreElements/actual.js
@@ -0,0 +1,14 @@
+const Foo = () => (
+  <div>
+    <div>
+      <span>
+        <test>
+          <Bar1 />
+          <Bar2>
+            <Bar3 />
+          </Bar2>
+        </test>
+      </span>
+    </div>
+  </div>
+);
---
+++ test/fixtures/options-additionalIgnoreElements/expected.js
@@ -0,0 +1,14 @@
+const Foo = () => (
+  <div data-test-id="Foo">
+    <div>
+      <span>
+        <test>
+          <Bar1 data-test-id="Foo-Bar1" />
+          <Bar2 data-test-id="Foo-Bar2">
+            <Bar3 data-test-id="Foo-Bar3" />
+          </Bar2>
+        </test>
+      </span>
+    </div>
+  </div>
+);
