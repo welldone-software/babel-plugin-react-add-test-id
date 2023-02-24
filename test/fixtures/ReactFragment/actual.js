009f206b636368004f2ee6e7ff01796e8f17323a
yujiaomo <yujiao.mo@envision-digital.com>
2023-02-24 10:38:02 +0800

    Add React Fragment test case

---
+++ test/fixtures/ReactFragment/actual.js
@@ -0,0 +1,9 @@
+const Foo = ({ children }) => (
+    <>
+        <React.Fragment>
+            <Fragment>
+                <div>Hi</div>
+            </Fragment>
+        </React.Fragment>
+    </>
+);
---
+++ test/fixtures/ReactFragment/expected.js
@@ -0,0 +1,9 @@
+const Foo = ({ children }) => (
+    <>
+        <React.Fragment>
+            <Fragment>
+                <div data-bd-fe-id="Hi">Hi</div>
+            </Fragment>
+        </React.Fragment>
+    </>
+);
