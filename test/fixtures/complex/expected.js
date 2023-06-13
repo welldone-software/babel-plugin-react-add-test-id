9f5554555d9497e966ee084f5289e023fe7918b3
ken.cheung <ken.cheung@scmp.com>
2023-06-13 12:00:47 +0800

    fix: change implementation on ignore elements

--- test/fixtures/complex/expected.js
+++ test/fixtures/complex/expected.js
@@ -1,7 +1,7 @@
 const Foo = ({ className, children }) => {
   const x = 2;
   return (
-    <div className={className} data-test-id="Foo">
+    <div className={className}>
       <div>
         <Bar data-test-id="Foo-Bar">{children}</Bar>
       </div>
--- test/fixtures/nested/expected.js
+++ test/fixtures/nested/expected.js
@@ -1,7 +1,7 @@
 const Foo = () => (
-  <div data-test-id="Foo">
+  <div>
     <Bar data-test-id="Foo-Bar" />
   </div>
 );
 
-const Bar = () => <div data-test-id="Bar">Hi</div>;
+const Bar = () => <div>Hi</div>;
